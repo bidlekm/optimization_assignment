634359dcffce81f7301ea98e0bddfcb9ac26e373
rafrodvel <rafaelrodriguezvelasco@gmail.com>
2023-11-19 21:27:02 +0100

    Task 1 and 2 pseudo-finished

--- balance_function.m
+++ balance_function.m
@@ -1,6 +1,9 @@
 function n = balance_function(a,dhdx,g,rho,x,A,H_obs)
-    fun = @(n)norm(nthroot((-n+2)./(2.*A).*abs(dhdx).^(1-n).*dhdx.^-1.*a ,n+2)- H_obs,2);
-    n0 = 3;
 
+    fun = @(n) sqrt(sum((((-n+2)./(2*A).*abs(dhdx).^(1-n).*dhdx.^(-1).*a).^1/(n+2)- H_obs).^2));
+    n0 = 4;
+    fun(1:4)
+    plot(1:4, fun(1:4))
     [n] = fminunc(fun,n0);
+
 end
