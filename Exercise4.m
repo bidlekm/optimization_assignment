3f308481e46288dec0214353068f00108992cbc5
rafrodvel <rafaelrodriguezvelasco@gmail.com>
2023-11-20 17:49:53 +0100

    I hate my life

--- Exercise4.m
+++ Exercise4.m
@@ -9,7 +9,7 @@ H = @(n, A) (-(2+n)/(2*A).*a.*(rho*g).^(-n).*abs(dhdx).^(1-n).*dhdx.^-1).^(1./(n
 sum1 = @(n, A) sum((H(n,A)- H_obs).^2);
 
 fun = @(args) sqrt(sum1(args(1),args(2)));
-initialdata = [1.1, 1e-24];
+initialdata = [2.89, 2.09e-26];
 
 
 
@@ -17,19 +17,18 @@ options = optimoptions('fminunc');
 options.TolFun = 1e-35;
 options.OptimalityTolerance = 1e-35;    % This completely sucks
 options.StepTolerance = 1e-35;
-options.MaxFunctionEvaluations = 5e4;
+options.MaxFunctionEvaluations = 5e8;
+options.MaxIterations = 1e8;
 [minParams, fval] = fminunc(fun, initialdata, options);
-minParams
-fval
 
 
 
 
+%%
 
 
-
-n_values = linspace(2, 3, 100);
-A_values = linspace(1e-22, 1e-26, 100);
+n_values = linspace(2.5, 4, 1000);
+A_values = linspace(0.01e-25, 0.00001e-25, 1000);
 
 [NN, AA] = meshgrid(n_values, A_values);
 Z = zeros(size(NN));
@@ -38,9 +37,19 @@ for i = 1:numel(NN)
     Z(i) = fun([NN(i), AA(i)]);
 end
 
-figure;
-surf(NN, log10(AA), Z);
-xlabel('n');
-ylabel('log_{10}(A)');
-zlabel('Function Value');
-title('Plot of the Function');
+zmin = min(Z(:));
+[row, col] = find(Z == zmin);
+nmin = n_values(col)
+Amin = A_values(row)
+fun([nmin, Amin])
+
+% Real values
+% Amin = 1.5051e-26, nmin = 2.90394
+
+% figure;
+% surf(NN, AA, Z);
+% xlabel('n');
+% ylabel('log_{10}(A)');
+% zlabel('Function Value');
+% title('Plot of the Function');
+
